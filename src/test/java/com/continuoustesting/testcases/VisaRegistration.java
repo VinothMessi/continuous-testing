4ffa6b3fa2ead3ba41326ebf39c9893ceca46098
vinothmessi <vinothone2one@gmail.com>
2021-08-18 00:33:18 +0530

    fixed error

--- src/test/java/com/continuoustesting/testcases/VisaRegistration.java
+++ src/test/java/com/continuoustesting/testcases/VisaRegistration.java
@@ -8,7 +8,7 @@ public class VisaRegistration extends BaseTest {
     @Test
     public void visaRegistration() {
         browser.manage().window().maximize();
-        browser.get(app.getUrl());
+        browser.get(app.getAppUrl());
         System.out.println("Page Title:" + browser.getTitle());
         browser.quit();
     }
