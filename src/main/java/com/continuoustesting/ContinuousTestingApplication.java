d02e4d463b0b5e3c2743044fb6a05053126743e3
Vinoth Murali <vinothone2one@gmail.com>
2021-08-17 16:11:12 +0530

    Spring-Boot project initialized

---
+++ src/main/java/com/continuoustesting/ContinuousTestingApplication.java
@@ -0,0 +1,13 @@
+package com.continuoustesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousTestingApplication.class, args);
+	}
+
+}
