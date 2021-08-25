1f6dc6de2b559da1cb59031e13771011192e45c7
vinothmessi <vinothone2one@gmail.com>
2021-08-25 23:25:15 +0530

    removed Jenkinsfile

--- src/main/java/com/continuoustesting/driverfactory/SelenoidBrowsers.java
+++ src/main/java/com/continuoustesting/driverfactory/SelenoidBrowsers.java
@@ -20,64 +20,68 @@ import org.springframework.context.annotation.Profile;
 @Lazy
 @Configuration
 @Profile("selenoid")
-public class SelenoidBrowsers {
-        @Autowired
-        private HubDetails hub;
+public
+class SelenoidBrowsers {
+    @Autowired
+    private HubDetails hub;
 
-        @Lazy
-        @Bean
-        @ConditionalOnProperty(name = "browser", havingValue = "chrome")
-        public WebDriver selenoidChrome() throws MalformedURLException {
-                var capabilities = new DesiredCapabilities();
-                capabilities.setCapability(Selenoid.BROWSER.label, "chrome");
-                capabilities.setCapability(Selenoid.VERSION.label, hub.getVersion());
-                capabilities.setCapability(Selenoid.VNC.label, hub.isEnableVNC());
-                capabilities.setCapability(Selenoid.VIDEO.label, hub.isEnableVideo());
-                capabilities.setCapability(Selenoid.VIDEO_NAME.label,
-                                hub.getVideoFile() + "_" + Utilities.currentDateTime() + hub.getVideoFormat());
-                capabilities.setCapability(Selenoid.LOG.label, hub.isEnableLog());
-                capabilities.setCapability(Selenoid.LOG_NAME.label,
-                                hub.getLogFile() + "_" + Utilities.currentDateTime() + hub.getLogFormat());
-                return new RemoteWebDriver(new URL(
-                                hub.getProtocol() + "://" + hub.getHost() + ":" + hub.getPort() + hub.getEndpoint()),
-                                capabilities);
-        }
+    @Lazy
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
+    public
+    WebDriver selenoidChrome() throws MalformedURLException {
+        var capabilities = new DesiredCapabilities();
+        capabilities.setCapability(Selenoid.BROWSER.label, "chrome");
+        capabilities.setCapability(Selenoid.VERSION.label, hub.getVersion());
+        capabilities.setCapability(Selenoid.VNC.label, hub.isEnableVNC());
+        capabilities.setCapability(Selenoid.VIDEO.label, hub.isEnableVideo());
+        capabilities.setCapability(Selenoid.VIDEO_NAME.label,
+                hub.getVideoFile() + "_" + Utilities.currentDateTime() + hub.getVideoFormat());
+        capabilities.setCapability(Selenoid.LOG.label, hub.isEnableLog());
+        capabilities.setCapability(Selenoid.LOG_NAME.label,
+                hub.getLogFile() + "_" + Utilities.currentDateTime() + hub.getLogFormat());
+        return new RemoteWebDriver(new URL(
+                hub.getProtocol() + "://" + hub.getHost() + ":" + hub.getPort() + hub.getEndpoint()),
+                capabilities);
+    }
 
-        @Lazy
-        @Bean
-        @ConditionalOnProperty(name = "browser", havingValue = "firefox")
-        public WebDriver selenoidFirefox() throws MalformedURLException {
-                var capabilities = new DesiredCapabilities();
-                capabilities.setCapability(Selenoid.BROWSER.label, "firefox");
-                capabilities.setCapability(Selenoid.VERSION.label, hub.getVersion());
-                capabilities.setCapability(Selenoid.VNC.label, hub.isEnableVNC());
-                capabilities.setCapability(Selenoid.VIDEO.label, hub.isEnableVideo());
-                capabilities.setCapability(Selenoid.VIDEO_NAME.label,
-                                hub.getVideoFile() + "_" + Utilities.currentDateTime() + hub.getVideoFormat());
-                capabilities.setCapability(Selenoid.LOG.label, hub.isEnableLog());
-                capabilities.setCapability(Selenoid.LOG_NAME.label,
-                                hub.getLogFile() + "_" + Utilities.currentDateTime() + hub.getLogFormat());
-                return new RemoteWebDriver(new URL(
-                                hub.getProtocol() + "://" + hub.getHost() + ":" + hub.getPort() + hub.getEndpoint()),
-                                capabilities);
-        }
+    @Lazy
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public
+    WebDriver selenoidFirefox() throws MalformedURLException {
+        var capabilities = new DesiredCapabilities();
+        capabilities.setCapability(Selenoid.BROWSER.label, "firefox");
+        capabilities.setCapability(Selenoid.VERSION.label, hub.getVersion());
+        capabilities.setCapability(Selenoid.VNC.label, hub.isEnableVNC());
+        capabilities.setCapability(Selenoid.VIDEO.label, hub.isEnableVideo());
+        capabilities.setCapability(Selenoid.VIDEO_NAME.label,
+                hub.getVideoFile() + "_" + Utilities.currentDateTime() + hub.getVideoFormat());
+        capabilities.setCapability(Selenoid.LOG.label, hub.isEnableLog());
+        capabilities.setCapability(Selenoid.LOG_NAME.label,
+                hub.getLogFile() + "_" + Utilities.currentDateTime() + hub.getLogFormat());
+        return new RemoteWebDriver(new URL(
+                hub.getProtocol() + "://" + hub.getHost() + ":" + hub.getPort() + hub.getEndpoint()),
+                capabilities);
+    }
 
-        @Lazy
-        @Bean
-        @ConditionalOnProperty(name = "browser", havingValue = "edge")
-        public WebDriver selenoidEdge() throws MalformedURLException {
-                var capabilities = new DesiredCapabilities();
-                capabilities.setCapability(Selenoid.BROWSER.label, "MicrosoftEdge");
-                capabilities.setCapability(Selenoid.VERSION.label, hub.getVersion());
-                capabilities.setCapability(Selenoid.VNC.label, hub.isEnableVNC());
-                capabilities.setCapability(Selenoid.VIDEO.label, hub.isEnableVideo());
-                capabilities.setCapability(Selenoid.VIDEO_NAME.label,
-                                hub.getVideoFile() + "_" + Utilities.currentDateTime() + hub.getVideoFormat());
-                capabilities.setCapability(Selenoid.LOG.label, hub.isEnableLog());
-                capabilities.setCapability(Selenoid.LOG_NAME.label,
-                                hub.getLogFile() + "_" + Utilities.currentDateTime() + hub.getLogFormat());
-                return new RemoteWebDriver(new URL(
-                                hub.getProtocol() + "://" + hub.getHost() + ":" + hub.getPort() + hub.getEndpoint()),
-                                capabilities);
-        }
+    @Lazy
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "edge")
+    public
+    WebDriver selenoidEdge() throws MalformedURLException {
+        var capabilities = new DesiredCapabilities();
+        capabilities.setCapability(Selenoid.BROWSER.label, "MicrosoftEdge");
+        capabilities.setCapability(Selenoid.VERSION.label, hub.getVersion());
+        capabilities.setCapability(Selenoid.VNC.label, hub.isEnableVNC());
+        capabilities.setCapability(Selenoid.VIDEO.label, hub.isEnableVideo());
+        capabilities.setCapability(Selenoid.VIDEO_NAME.label,
+                hub.getVideoFile() + "_" + Utilities.currentDateTime() + hub.getVideoFormat());
+        capabilities.setCapability(Selenoid.LOG.label, hub.isEnableLog());
+        capabilities.setCapability(Selenoid.LOG_NAME.label,
+                hub.getLogFile() + "_" + Utilities.currentDateTime() + hub.getLogFormat());
+        return new RemoteWebDriver(new URL(
+                hub.getProtocol() + "://" + hub.getHost() + ":" + hub.getPort() + hub.getEndpoint()),
+                capabilities);
+    }
 }
